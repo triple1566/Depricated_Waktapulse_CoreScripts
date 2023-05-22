83a0c91eb208872cc432d1d20badf23754d95957
triple1566 <ljhub1566@gmail.com>
2023-05-22 01:24:31 -0300

    Created a gunfire script

--- Weapon/GunfireBasic.cs
+++ Weapon/GunfireBasic.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//TODO: make the gun aim where crosshair is aiming at
+//      make the gun fire
+
 public class GunfireBasic : MonoBehaviour
 {
     // Start is called before the first frame update
